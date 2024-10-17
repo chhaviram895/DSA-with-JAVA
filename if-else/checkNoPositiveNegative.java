fc331dde77c802645099d5f65addad985fa9e57d
chhaviram895 <ckushwah895@gmail.com>
2024-10-17 07:17:25 +0530

    Add Check No Positive Negative

---
+++ if-else/checkNoPositiveNegative.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class checkNoPositiveNegative {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n = sc.nextInt();
+    if(n>0){
+        System.out.println("The number is positive."); }
+        else if(n<0){System.out.println("The nuber is negative.");}
+        else{System.out.println("Zero");}
+   }
+}
