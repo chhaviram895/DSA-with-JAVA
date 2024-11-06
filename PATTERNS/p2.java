40443503470f9ba6d58e654f06cd9594bb76ff2b
chhaviram895 <ckushwah895@gmail.com>
2024-11-06 12:32:20 +0530

    Pattern p2 ...

--- PATTERNS/p2.java
+++ PATTERNS/p2.java
@@ -1,21 +1,20 @@
-// *
-// *       *
-// *       *       *
-// *       *       *       *
-// *       *       *       *       *
+// 1
+// 2       2
+// 3       3       3
+// 4       4       4       4
+// 5       5       5       5      5
 
 import java.util.Scanner;
 
-public class p1 {
+public class p2 {
     public static void main(String[] args) {
         Scanner ram = new Scanner(System.in);
         System.out.println("Enter an number..");
         int n = ram.nextInt();
         System.out.println("Result==>");
-
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*\t");
+                System.out.print(i + "\t");
             }
             System.out.println();
         }
