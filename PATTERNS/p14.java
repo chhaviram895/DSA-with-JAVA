0058780ef07caca7667f9284ee61492b5a62dfef
chhaviram895 <ckushwah895@gmail.com>
2024-11-07 17:00:04 +0530

    Patterns p14....

---
+++ PATTERNS/p14.java
@@ -0,0 +1,32 @@
+//                                 *
+//                         *       *       *
+//                 *       *       *       *       *        
+//         *       *       *       *       *       *       *
+// *       *       *       *       *       *       *       *       *
+//         *       *       *       *       *       *       *
+//                 *       *       *       *       *
+//                         *       *       *
+//                                 *
+import java.util.Scanner;
+public class p14{
+    public static void main(String[] args) {
+        Scanner ram = new Scanner(System.in);
+        System.out.println("Enter an number..");
+        int n = ram.nextInt();
+        System.out.println("Result==>");
+        int nst= 1;int nsp =n-1;
+
+        for (int i = 1; i <=2*n-1; i++) {
+
+            for (int j = 1; j <=nsp; j++) {
+                System.out.print(" \t");
+            }
+            for (int j = 1; j <=nst; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+            if(i<n){nsp--; nst+=2;}
+            else{nsp++; nst-=2;}
+        }
+    }
+}
