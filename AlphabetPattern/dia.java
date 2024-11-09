13317591282e057f8832d8fdd13b2a6cf82ba82b
chhaviram895 <ckushwah895@gmail.com>
2024-11-09 10:42:18 +0530

    Patterns p4 dia...

--- AlphabetPattern/dia.java
+++ AlphabetPattern/dia.java
@@ -1,3 +1,32 @@
+//  *
+//    *
+//      *            
+//        *
+//          *        
+//            *      
+//              *    
+//                *  
+//                  *
+import java.util.Scanner;
 public class dia {
-    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter an no...");
+        int n = sc.nextInt();
+        System.out.println("Result-->");
+
+        int nst = n;
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=nst;j++){
+                if(j==i){
+                    System.out.print(" *");
+                }
+                else{
+                    System.out.print("  ");
+                }    
+            }
+            System.out.println();
+    }
+}
 }
