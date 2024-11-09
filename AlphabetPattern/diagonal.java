2f8d1f9358f6afbe99441128f0bac58ab7d4daef
chhaviram895 <ckushwah895@gmail.com>
2024-11-09 10:38:16 +0530

    Update...

--- AlphabetPattern/diagonal.java
+++ AlphabetPattern/diagonal.java
@@ -1,3 +1,12 @@
+// * * * * * * * * *
+// * *             *
+// *   *           *
+// *     *         *
+// *       *       *
+// *         *     *
+// *           *   *
+// *             * *
+// * * * * * * * * *
 import java.util.Scanner;
 public class diagonal {
     public static void main(String[] args) {
