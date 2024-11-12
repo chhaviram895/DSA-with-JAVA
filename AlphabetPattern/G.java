034b2925e88f99f7809caf945a0165d8b4c3c103
chhaviram895 <ckushwah895@gmail.com>
2024-11-12 14:49:17 +0530

    Patterns p13 G...

---
+++ AlphabetPattern/G.java
@@ -0,0 +1,32 @@
+// * * * * * * * * *
+// *
+// *                 
+// *                 
+// *       * * * * * 
+// *       *       *
+// *       *       * 
+// *       *       * 
+// * * * * *       *
+import java.util.Scanner;
+public class G{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter an no...");
+        int n = sc.nextInt();
+        System.out.println("Result-->");
+
+        int nst = n;
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=nst;j++){
+                if(i==1||j==1||j==n && i>=n/2+1||i==n/2+1 && j>=n/2+1||j==n/2+1 && i>=n/2+1 ||j==n && i>=n/2+1||i==n && j<=n/2+1)
+                System.out.print("* ");
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
