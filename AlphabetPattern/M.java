ece8130fd03abf04e685fc671800b0942fc18f4c
chhaviram895 <ckushwah895@gmail.com>
2024-11-13 12:02:01 +0530

    Patterns p19 M...

---
+++ AlphabetPattern/M.java
@@ -0,0 +1,33 @@
+// *               *
+// * *           * * 
+// *   *       *   * 
+// *     *   *     * 
+// *       *       * 
+// *               * 
+// *               * 
+// *               * 
+// *               *
+
+import java.util.Scanner;
+public class M{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter an no...");
+        int n = sc.nextInt();
+        System.out.println("Result-->");
+
+        int nst = n;
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=nst;j++){
+                if(j==1 || j==n || i==j && i<=n/2+1 || i+j==n+1 && i<=n/2+1)
+                System.out.print("* ");
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
