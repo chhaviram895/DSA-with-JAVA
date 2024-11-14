a5fa7ee3993a726ff12968f36b4d81c22bd8cc62
chhaviram895 <ckushwah895@gmail.com>
2024-11-14 12:20:22 +0530

    Arrays acess an ele...

---
+++ Arrays/Access.java
@@ -0,0 +1,7 @@
+// How do you access an element in an array in Java?
+public class Access {
+public static void main(String[] args) {
+    int [] arr = {1, 2, 3, 4, 5};
+    System.out.println( "Result-->"+" "+ arr[3]);
+}   
+}
