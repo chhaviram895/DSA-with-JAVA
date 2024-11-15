776221b9fc9a31c4b7236fa3be1ab34e60b13f7e
chhaviram895 <ckushwah895@gmail.com>
2024-11-15 17:19:46 +0530

    Arrays Length....

---
+++ Arrays/Length.java
@@ -0,0 +1,7 @@
+// How do you find the length of an array in Java?
+public class Length {
+   public static void main(String[] args) {
+    int[] arr = {0,1,2,3,4,5,6,7,8};
+    System.out.println(arr.length);
+   } 
+}
