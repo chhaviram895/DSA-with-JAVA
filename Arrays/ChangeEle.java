528354292ca344838b241a8a36460fc52103bb21
chhaviram895 <ckushwah895@gmail.com>
2024-11-15 17:11:26 +0530

    Arrays Change an Element..

---
+++ Arrays/ChangeEle.java
@@ -0,0 +1,9 @@
+// How do you change an element in an array in Java?
+public class ChangeEle {
+public static void main(String[] args) {
+    int[]arr={1,2,3,4,5};
+    System.out.println(arr[4]);
+    arr[4]=10;
+    System.out.println(arr[4]);
+} 
+}
