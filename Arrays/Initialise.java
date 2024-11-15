720b7c1785b6d7e8322046b3bfe24395ae797c8e
chhaviram895 <ckushwah895@gmail.com>
2024-11-15 17:28:08 +0530

    Arrays Iterate...

--- Arrays/Initialise.java
+++ Arrays/Initialise.java
@@ -1,7 +1,7 @@
 // How do you initialise an array in Java?
 public class Initialise {
- public static void main(String[] args) {
-    int [] arr;
-    arr = new int[8];
- }   
+   public static void main(String[] args) {
+      int[] arr;
+      arr = new int[8];
+   }
 }
