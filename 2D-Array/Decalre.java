7d8f468265e3eed60d06d18bf8db0a8ddfc95fbd
chhaviram895 <ckushwah895@gmail.com>
2024-12-14 13:17:07 +0530

    Declare 2-D Array.....

---
+++ 2D-Array/Decalre.java
@@ -0,0 +1,14 @@
+
+// How do you declare a 2D array in Java?
+import java.util.Scanner;
+
+public class Decalre {
+    public static void main(String[] args) {
+        Scanner ram = new Scanner(System.in);
+        System.out.println("jkl");
+        int rows = ram.nextInt();
+        int columns = ram.nextInt();
+        int[][] arr = new int[rows][columns];
+        System.out.println();
+    }
+}
