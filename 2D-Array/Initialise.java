8ea4ba51f8bb179a4709ac69e7bf4bcba3f21ea4
chhaviram895 <ckushwah895@gmail.com>
2024-12-14 13:24:12 +0530

    2-D Array Initialise....

---
+++ 2D-Array/Initialise.java
@@ -0,0 +1,8 @@
+// How do you initialise a 2D array with specific values?
+public class Initialise {
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] arr1 = new int[2][2];
+        System.out.println(arr[2][1]);
+    }
+}
