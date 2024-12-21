0864f02897d18a6ee97dea09ac75dd9788f32de8
chhaviram895 <ckushwah895@gmail.com>
2024-12-21 16:43:32 +0530

    2-D Array Lower Diagonal.......

---
+++ 2D-Array/LowerDiagonal.java
@@ -0,0 +1,21 @@
+// Lower Diagonal Traversal in 2d array 
+public class LowerDiagonal {
+    public static void main(String[] args) {
+        int[][] arr = { { 00, 01, 02, 03, 04 },
+                { 10, 11, 12, 13, 14 },
+                { 20, 21, 22, 23, 24 },
+                { 30, 31, 32, 33, 34 },
+                { 40, 41, 42, 43, 44 } };
+
+        for (int k = 0; k < arr.length; k++) {
+            for (int r = 0; r < arr.length; r++) {
+                for (int c = 0; c < arr[0].length; c++) {
+                    if (r  == c + k) {
+                        System.out.print(arr[r][c] + " ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
